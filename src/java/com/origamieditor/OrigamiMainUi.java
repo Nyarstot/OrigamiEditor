748350b6f83ccb3c88d730ed1e037e139eee489f
Nikita Kozlov <Nyarstot@protonmail.com>
2021-09-09 21:06:53 +0300

    Application user interface update

--- src/java/com/origamieditor/OrigamiMainUi.java
+++ src/java/com/origamieditor/OrigamiMainUi.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 public class OrigamiMainUi extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(OrigamiMainUi.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(OrigamiMainUi.class.getResource("OrigamiMain-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 
         stage.setTitle("Origami Editor");
